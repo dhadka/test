ffa96d0e26c3301eec4ad3025d89b1c159298e2c
Andrew <andrewdircks@me.com>
2021-01-21 13:10:53 -0500

    use add-and-commit action

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -36,7 +36,6 @@ def page(url, output):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)\
             AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'}
     response = requests.get(url, headers=headers)
-    print(response.text)
     soup = BeautifulSoup(response.content, 'lxml')
     empty = False
     for item in soup.select('[data-lid]'):
